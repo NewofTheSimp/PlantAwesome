e851b2b8a58ee4b22a28126ff228bc8fb9adccdf
NewofTheSimp <wvinke550@gmail.com>
2025-06-12 11:54:23 +0200

    Added animations for the menu

--- header.php
+++ header.php
@@ -8,19 +8,19 @@
 
     </p>
     <p class="header-cart">
-        <a href="index.php">Home</a>
-        <a href="cart.php">Cart</a>
+        <a class="header-a" href="index.php">Home</a>
+        <a class="header-a" href="cart.php">Cart</a>
         <?php if (isset($_SESSION['UserName'])) { ?>
-            <a href="order.php">Order</a>
-            <a href="index.php"><?php echo $_SESSION['UserName'] ?></a>
-            <a href="log-out.php">Log Out</a>
-            <a href="user.php">Account page</a>
+            <a class="header-a" href="order.php">Order</a>
+            <a class="header-a" href="index.php"><?php echo $_SESSION['UserName'] ?></a>
+            <a class="header-a" href="log-out.php">Log Out</a>
+            <a class="header-a" href="user.php">Account page</a>
         <?php } else { ?>
-            <a href="log-in.php">Login</a>
-            <a href="create-account.php">Create Account</a>
+            <a class="header-a" href="log-in.php">Login</a>
+            <a class="header-a" href="create-account.php">Create Account</a>
         <?php } ?>
         <?php if (isset($_SESSION['IsAdmin']) && $_SESSION['IsAdmin'] === 1) { ?>
-            <a href="admin.php">Admin</a>
+            <a class="header-a" href="admin.php">Admin</a>
         <?php } ?>
     </p>
 </header>
